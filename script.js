6f0894cf53ae1decf7ef767b8b0389624ab3179b
Jesús Quiroz Rojas <142179535+JesusCK@users.noreply.github.com>
2024-06-02 18:53:01 -0500

    update

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
+console.log(navLinks, navbar)
+
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -10,11 +12,14 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
+        console.log(id)
+
         if (top >= offset && top < offset + height){
             navLinks.forEach(links => {
+                console.log(links)
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id 
-                + ']').classList.add('active');
+                console.log(links)
+                document.querySelector('header nav a[href*=' + id + ' ]').classList.add('active');
             })
         }
 
@@ -23,5 +28,9 @@ window.onscroll = () => {
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
-    menuIcon.classList.toggle('active');
+    if (navbar.style.display === 'block') {
+        navbar.style.display = 'none';
+    } else {
+        navbar.style.display = 'block';
+    }
 }
